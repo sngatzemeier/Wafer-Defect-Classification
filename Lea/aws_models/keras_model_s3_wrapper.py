ae05792543d468c9f952ba01b527bcb059823ee9
EC2 Default User <ec2-user@ip-172-16-51-185.us-west-2.compute.internal>
2022-03-06 19:27:34 +0000

    updates 3/6

--- Lea/aws_models/keras_model_s3_wrapper.py
+++ Lea/aws_models/keras_model_s3_wrapper.py
@@ -49,3 +49,31 @@ def s3_get_keras_model(model_name: str) -> keras.Model:
         zip_ref.extractall(f"{tempdir}/{model_name}")
     # Load the keras model from the temporary directory
     return keras.models.load_model(f"{tempdir}/{model_name}")
+
+import logging
+import boto3
+from botocore.exceptions import ClientError
+import os
+
+
+def s3_upload_file(file_name, bucket, object_name=None):
+    """Upload a file to an S3 bucket
+
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = os.path.basename(file_name)
+
+    # Upload the file
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
